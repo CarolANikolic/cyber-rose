3eaf71b6cf557e6caa5ef06cf679c9f19b8769e0
Carol A. Nikolic <carolnikolic@gmail.com>
2022-10-21 17:15:21 -0300

    adding functionality to buttremoving button on main, adjusting text in footer and adding fuctionality to buttons

---
+++ script.js
@@ -0,0 +1,41 @@
+// Functions for opening html file and accessing the link
+
+function OpenOne() {
+    window.open('flower-one.html');
+     }
+
+function OpenTwo() {
+    window.open('flower-two.html');
+     }
+
+function OpenThree() {
+    window.open('flower-three.html');
+     }
+
+function OpenFour() {
+    window.open('flower-four.html');
+    }
+
+function OpenFive() {
+    window.open('flower-five.html');
+    }
+
+function OpenSix() {
+    window.open('flower-six.html');
+    }
+
+function OpenSeven() {
+    window.open('flower-seven.html');
+    }
+
+function OpenEight() {
+    window.open('flower-eight.html');
+    }
+
+function OpenNine() {
+    window.open('flower-nine.html');
+    }
+
+function OpenTen() {
+    window.open('flower-ten.html');
+    }
